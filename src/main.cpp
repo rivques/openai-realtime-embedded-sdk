1bb26c641c8aaa111a60ef2fe636291579f1aa5a
rivques <38469076+rivques@users.noreply.github.com>
2025-02-14 21:46:08 -0500

    so close to greatness

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,15 @@
 #include <esp_log.h>
 #include <peer.h>
 
+
+#include "display_handler.h"
+#include "time_mgmt.h"
+#include "freertos/FreeRTOS.h"
+
 #ifndef LINUX_BUILD
 #include "nvs_flash.h"
+
+const char *TAG = "MAIN";
 
 extern "C" void app_main(void) {
   esp_err_t ret = nvs_flash_init();
@@ -15,6 +22,17 @@ extern "C" void app_main(void) {
     ret = nvs_flash_init();
   }
   ESP_ERROR_CHECK(ret);
+
+  ESP_LOGI(TAG, "Initializing display handler");
+  init_all_display();
+  ESP_LOGI(TAG, "Display handler initialized");
+  update_time_display(12, 0, 0);
+  update_date_display("Jan", "01", "2023", "Mon");
+  update_status_bar("12:00:00PM", 70);
+  ESP_LOGI(TAG, "Initializing display update task");
+  xTaskCreatePinnedToCore(update_display_task, "update_display_task", 65536,
+                          NULL, 5, NULL, 1);
+  ESP_LOGI(TAG, "Display update task initialized");
 
   ESP_ERROR_CHECK(esp_event_loop_create_default());
   peer_init();
---
+++ src/pixeboy_32.h
@@ -0,0 +1,8 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "lvgl.h"
+extern const lv_font_t pixeboy_32;
+#ifdef __cplusplus
+}
+#endif
